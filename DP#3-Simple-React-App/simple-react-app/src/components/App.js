97d76f44bdfb942d3e0fa43f0db4497cab29ec59
cbsardina <cbsardina@gmail.com>
2017-10-22 16:55:33 -0400

    DP#3: Simple react app initial commit.

---
+++ DP#3-Simple-React-App/simple-react-app/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+            <div className="my-header">
+                EarthQuakes!
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
