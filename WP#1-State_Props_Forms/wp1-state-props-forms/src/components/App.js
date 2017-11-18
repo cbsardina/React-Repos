2e3c7bf462088de53da36486efc1213f010d6e2a
cbsardina <cbsardina@gmail.com>
2017-11-18 18:04:04 -0500

    WP#1: Organizing files, completing project setup.

--- WP#1-State_Props_Forms/wp1-state-props-forms/src/components/App.js
+++ WP#1-State_Props_Forms/wp1-state-props-forms/src/components/App.js
@@ -19,3 +19,17 @@ class App extends Component {
 }
 
 export default App;
+
+/*  * * * * * INSTRUCTIONS
+This project is about managing state, props, lifecycles, and forms. The following should be true of your application:
+
+1. Your PlayListcomponent should pass down the state in the form of props to your PlayListItem component which will render the actual play list entries. (Hint: PlayListItem should be imported in PlayList)
+
+2. Your App.js component should only have import statements for your NavBar, PlayList, and PlayListForm.
+
+3. The state for your PlayListForm component should have userName, songArtist, songTitle, and songNotes in order to function properly with the fetch functions from the tinylasgna.js file that you will copy into the component. Follow the directions in that file.
+
+4. The state for your PlayList should just have one key value pair, songs, in order to properly function with the fetch function from the tinylasagna.js file that you will copy into the PlayListComponent (follow the directions in that file).
+
+5. The styling is up to you, but please make this a polished product!
+ */
